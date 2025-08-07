f9c2bab5918be3080b7765bd1162036c98b90a6a
Laima Lukoseviciute <lmlukoseviciute@gmail.com>
2025-08-07 13:53:58 +0300

    2 models task 1

---
+++ utils.py
@@ -0,0 +1,19 @@
+def get_season(month):
+    if month in [12, 1, 2]:
+        return "winter"
+    elif month in [3, 4, 5]:
+        return "spring"
+    elif month in [6, 7, 8]:
+        return "summer"
+    else:
+        return "autumn"
+
+def get_time_of_day(hour):
+    if 6 <= hour < 12:
+        return "morning"
+    elif 12 <= hour < 18:
+        return "afternoon"
+    elif 18 <= hour < 24:
+        return "evening"
+    else:
+        return "night"
